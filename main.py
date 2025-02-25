145ed27fd026d27f79020c284367518526c186c7
Benno Weber <bennopaul.weber@gmail.com>
2025-02-25 13:07:53 +0100

    V 1.1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("vlbelintrocrypto.hevs.ch", 6000))
 
-msg = "AB❤"
+msg = "AB ❤"
 
 uMsg = ""
 for i in msg:
@@ -12,7 +12,11 @@ for i in msg:
     uMsg += letter
 uMsg = uMsg.encode("UTF-8")
 
-print(b"ISCt\x00\x03"+uMsg)
+print(b"ISCt\x00\x04"+uMsg)
 
-s.sendall(b"ISCt\x00\x03"+uMsg)
-print(s.recv(1024))
+s.sendall(b"ISCt\x00\x04"+uMsg)
+recv = s.recv(1024)
+print(recv)
+
+
+
